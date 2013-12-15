302faab248cb4af5f80d487e97b07154a50fb7fa
bgnori <bgnori@gmail.com>
2013-12-15 11:01:09 +0900

    bring derived classes out of model. 	modified:   helper.py 	modified:   model.py 	renamed:    model.py -> objs.py

--- helper.py
+++ helper.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
 import model
+import objs
 
 model.init("/home/nori/Desktop/work/Trellonium/appkey.txt", "/home/nori/Desktop/work/Trellonium/secret.txt")
 
 app = model.theApp
 app.token = file("/home/nori/Desktop/work/Trellonium/token.txt").read().split()
 
-theBoard = model.BoardProxy("52a113d948daf8a31e0043dd")
+theBoard = objs.BoardProxy("52a113d948daf8a31e0043dd")
 xs = theBoard.lists()
 
