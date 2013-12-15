2c987f2aff1992b25cf82da505f624cc501d2366
bgnori <bgnori@gmail.com>
2013-12-15 11:43:59 +0900

    creating class with type()

---
+++ board.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from commons import objnames, dirname
+
+from model import TrelloProxy
+
+
+klassfields = dict(
+    path = "boards",
+    fields = set(["name", "desc", "descData", "closed", "idOrganization",
+        "invited", "pinned", "url", "prefs", "invitations", "memberships",
+        "shortLink", "subscribed", "labelNames", "powerUps", "dateLastActivity",
+        "dateLastView", "shortUrl"])
+    )
+
+BoardProxy = type('BoardProxy', (TrelloProxy, ), klassfields)
+
+BoardProxy.availables = TrelloProxy.build("    GET /1/members/[idMember or username]/[field]")
+TrelloProxy.register(BoardProxy)
+
+
+if __name__ == "__main__":
+    import model
+    model.init("/home/nori/Desktop/work/Trellonium/appkey.txt", "/home/nori/Desktop/work/Trellonium/secret.txt")
+    app = model.theApp
+    app.token = file("/home/nori/Desktop/work/Trellonium/token.txt").read().split()
+    theBoard = BoardProxy("52a113d948daf8a31e0043dd")
+    print theBoard
+    print theBoard.name()
+
