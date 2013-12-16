c24d5d5873e14112e34510eddfd1053c71164b05
Noriyuki Hosaka <bgnori@gmail.com>
2013-12-16 14:15:25 +0900

    Handler class

--- parsehtml.py
+++ parsehtml.py
@@ -6,37 +6,49 @@ from lxml import etree
 
 from commons import objnames, dirname
 
-def handle_argument(node):
-    print node.xpath('tt/span[@class="pre"]/text()')[0]
-    xs = node.xpath('ul/li[strong/text()="Valid Values:"]/ul/li/tt/span/text()')
-    if not xs:
-        xs = ['descriptive'] + node.xpath('ul/li[strong/text()="Valid Values:"]/text()')
-    print xs
 
+class Handler(object):
+    def __init__(self, name):
+        self.name = name
 
-def handle_node(node):
-    print "id:", node.attrib['id']
-    for s in node.xpath("h2"):
-        print "method:", s.text
-    for s in node.xpath("h2/span"):
-        print "path:", s.text
-    for m in node.xpath('ul/li[strong/text()="Required permissions:"]/text()'):
-        print 'permissions:', m
-    for m in node.xpath('ul/li[strong/text()="Arguments"]/ul/li'):
-        handle_argument(m)
-    #for m in n.xpath("ul/li/ul/li"):
-    #    print m.text
+    def handle(self, t):
+        self.handle_root(t)
+
+    def handle_root(self, t):
+        print t
+        for n in t.xpath("//div[@id=$id]/div", id=self.name):
+            print n
+            self.handle_node(n)
+
+    def handle_node(self, node):
+        print "id:", node.attrib['id']
+        for s in node.xpath("h2"):
+            print "method:", s.text
+        for s in node.xpath("h2/span"):
+            print "path:", s.text
+        for m in node.xpath('ul/li[strong/text()="Required permissions:"]/text()'):
+            print 'permissions:', m
+        for m in node.xpath('ul/li[strong/text()="Arguments"]/ul/li'):
+            self.handle_argument(m)
+        #for m in n.xpath("ul/li/ul/li"):
+        #    print m.text
+
+    def handle_argument(self, node):
+        print node.xpath('tt/span[@class="pre"]/text()')[0]
+        xs = node.xpath('ul/li[strong/text()="Valid Values:"]/ul/li/tt/span/text()')
+        if not xs:
+            xs = ['descriptive'] + node.xpath('ul/li[strong/text()="Valid Values:"]/text()')
+        print xs
 
 
 def process(name):
+    h = Handler(name)
     with file(os.path.join(dirname, name+".html"), 'r') as f:
         p = etree.HTMLParser()
         t = etree.parse(f, p)
-        for n in t.xpath("//*[@id=$id]/div", id=name):
-            print "-"*30
-            handle_node(n)
+        h.handle(t)
 
 for name in objnames:
     pass
 
-process("board")
+process("card")
