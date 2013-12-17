edf26748b170a22f040d679dadd764892520c2a8
Noriyuki Hosaka <bgnori@gmail.com>
2013-12-17 15:48:52 +0900

    case for token is not ready

--- helper.py
+++ helper.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
+import os.path
 import model
 import objs
 
 model.init("/home/nori/Desktop/work/Trellonium/appkey.txt", "/home/nori/Desktop/work/Trellonium/secret.txt")
 
 app = model.theApp
-app.token = file("/home/nori/Desktop/work/Trellonium/token.txt").read().split()
+token_loc = "/home/nori/Desktop/work/Trellonium/token.txt"
 
-theBoard = objs.BoardProxy("52a113d948daf8a31e0043dd")
-xs = theBoard.lists()
+if os.path.isfile(token_loc):
+    app.token = file(token_loc).read().split()
+    theBoard = objs.BoardProxy("52a113d948daf8a31e0043dd")
+    xs = theBoard.lists()
+    print xs
+else:
+    print app.token_url
+
 
